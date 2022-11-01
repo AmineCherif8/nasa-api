2cafb4c2393c260ea6e241a229270bd6d8820dff
Amine Cherif <amine.cherif@meritis.fr>
2022-11-01 12:36:18 +0100

    NASWD-002: Add a test for the rovers controller

--- backend/src/mars-rover/controller/mars-rover.controller.spec.ts
+++ backend/src/mars-rover/controller/mars-rover.controller.spec.ts
@@ -1,4 +1,4 @@
-import { HttpModule, HttpService } from "@nestjs/axios";
+import { HttpModule } from "@nestjs/axios";
 import { Test } from "@nestjs/testing";
 import { MarsRoverService } from "../service/mars-rover.service";
 import { MarsRoverController } from "./mars-rover.controller";
@@ -6,8 +6,8 @@ import { APP_INTERCEPTOR } from "@nestjs/core";
 import { CacheInterceptor, CacheModule } from "@nestjs/common";
 
 describe("RoversController", () => {
-  let catsController: MarsRoverController;
-  let catsService: MarsRoverService;
+  let marsRoverController: MarsRoverController;
+  let marsRoverService: MarsRoverService;
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       controllers: [MarsRoverController],
@@ -21,22 +21,22 @@ describe("RoversController", () => {
       imports: [CacheModule.register(), HttpModule],
     }).compile();
 
-    catsService = await moduleRef.get<MarsRoverService>(MarsRoverService);
-    catsController = await moduleRef.get<MarsRoverController>(
+    marsRoverService = await moduleRef.get<MarsRoverService>(MarsRoverService);
+    marsRoverController = await moduleRef.get<MarsRoverController>(
       MarsRoverController
     );
   });
 
   describe("getMarsRovers", () => {
     it("should return an array of rovers", async () => {
-      const result = await catsService.getAllMarsRover(
+      const result = await marsRoverService.getAllMarsRover(
         200,
         "FHAZ",
         "Curiosity"
       );
 
       expect(
-        await catsController.getMarsRover(200, "FHAZ", "Curiosity")
+        await marsRoverController.getMarsRover(200, "FHAZ", "Curiosity")
       ).toStrictEqual(result);
     });
   });
